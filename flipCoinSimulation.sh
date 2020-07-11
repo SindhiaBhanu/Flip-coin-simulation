de6fdf8ee64f21f46f55e20e67f4507ab11a9aca
SindhiaBhanu <sindhiabhanu518@gmail.com>
2020-07-11 07:14:33 +0530

    added code to find diffrence of 2 minimum points is achieved when it is a tie

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -1,7 +1,54 @@
 #!/bin/bash -x
+function repeat () {
 headCount=0
 tailCount=0
-while (($headCount!=21&&$tailCount!=21))
+while true
+do
+        for((i=0;i<$noOfTimes;i++))
+        do
+                result=$((RANDOM%2))
+                if [ $result = 0 ]
+                then
+                        ((headCount++))
+                        echo HEADS
+                else
+                        ((tailCount++))
+                        echo TAILS
+                fi
+        done
+        echo HEADS has won $headCount times
+        echo TAILS has won $tailCount times
+        if [[ $headCount -gt $tailCount ]]
+        then
+                difference=$(($headCount - $tailCount))
+                if [ $difference -ge 2 ]
+                then
+                        echo HEAD has WON by $difference times
+                        break
+                else
+                        continue
+                fi
+        elif [[ $headCount -lt $tailCount ]]
+        then
+                difference=$(($tailCount - $headCount))
+                if [ $difference -ge 2 ]
+                then
+                        echo TAIL has WON by $difference times
+                        break
+                else
+                        continue
+                fi
+        else
+                echo Its a TIE
+                continue
+        fi
+done
+}
+
+read -p "How many times you want to flip a coin" noOfTimes
+headCount=0
+tailCount=0
+for((i=0;i<$noOfTimes;i++))
 do
         result=$((RANDOM%2))
         if [ $result = 0 ]
@@ -25,4 +72,5 @@ then
         echo TAIL has WON by $difference times
 else
         echo Its a TIE
+        repeat
 fi
